34dd2d8d1fe0e4948e69ef8e0deb1d797eaf1c3e
Denis O <denis@universal-development.com>
2017-11-02 22:14:16 +0200

    Updated code formatting

--- android-test-app/src/main/java/com/unidev/app/testapp/activity/MainFragment.java
+++ android-test-app/src/main/java/com/unidev/app/testapp/activity/MainFragment.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) 2015,2016 Denis O <denis@universal-development.com>
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,6 +22,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.unidev.app.testapp.R;
 import com.unidev.polyfunciton.FunctionRequest;
@@ -65,7 +66,6 @@ public class MainFragment extends Fragment {
 
         return view;
     }
-
 
 
 }
